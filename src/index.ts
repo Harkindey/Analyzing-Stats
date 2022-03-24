239685faacdfa296ac22198a59ca1103370d9086
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2022-03-24 13:54:35 +0100

    initial analysis

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
 import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((item: string): string[] => {
+    return item.split(',');
+  });
+
+let manUnitedWin = 0;
+
+for (let match of matches) {
+  if (
+    (match[1] === 'Man United' && match[5] === 'H') ||
+    (match[2] === 'Man United' && match[5] === 'A')
+  ) {
+    manUnitedWin++;
+  }
+}
+console.log(`Man United won ${manUnitedWin} games`);
