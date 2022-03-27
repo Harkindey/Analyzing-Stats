8e34dae08a27a4f18110a6c28fd0d314badd9665
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2022-03-27 02:45:11 +0100

    CsvFileReader abstract class

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,12 @@
 import fs from 'fs';
 import { MatchResult } from './MatchResult';
-import { dateStringTodate } from './utils';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
-export class CsvFileReader {
+export abstract class CsvFileReader {
   data: MatchData[] = [];
 
   constructor(public filename: string) {}
+  abstract mapRow(row: string[]): MatchData;
 
   read(): void {
     this.data = fs
@@ -18,17 +18,5 @@ export class CsvFileReader {
         return item.split(',');
       })
       .map(this.mapRow);
-  }
-
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringTodate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6],
-    ];
   }
 }
