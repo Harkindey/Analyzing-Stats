f4febb6fa7dfdef54bd3cb092285e82f05451e6c
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2022-03-27 03:29:12 +0100

    Updating reader reference

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CSVFileReader';
 import { MatchResult } from './MatchResult';
+// Create an Object that satifies the data reader interface
+const csvFileReader = new CsvFileReader('football.csv');
+// Creat an instance of Match Reader and pass in something satisfying the  'Datareader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-const reader = new MatchReader('football.csv');
-reader.read();
-
+// Reference MatchReader.matches
 let manUnitedWin = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (
     (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
     (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
