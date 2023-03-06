b34c9b7d55c669bdaa738d7a4d9b0c5aa6e3d7b9
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-03-07 00:40:27 +0100

    WinAnalysis

--- src/initial/Analysers/WinsAnalysis.ts
+++ src/initial/Analysers/WinsAnalysis.ts
@@ -1,16 +1,16 @@
 import { MatchResult } from "../../MatchResult";
 import { MatchData } from "../MatchData";
-import { Analyser } from "../Sumarry";
+import { Analyser } from "../Summary";
 
 export class  WinsAnalysis implements Analyser {
  constructor(public team:string){}
- 
+
  run(matches:MatchData[]):string {
     let wins = 0;
     for (let match of matches) {
         if (
-            (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
-            (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
+            (match[1] === this.team && match[5] === MatchResult.HomeWin) ||
+            (match[2] === this.team && match[5] === MatchResult.AwayWin)
         ) {
             wins++;
         }
