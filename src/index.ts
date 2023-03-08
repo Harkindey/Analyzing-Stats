59c95e13d713079a305ed876e716b25ba46de17e
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-03-09 00:41:14 +0100

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { MatchReader } from "./initial/MatchReader";
 import { CsvFileReader } from "./initial/CsvFileReader";
-import { MatchResult } from "./MatchResult";
+import { ConsoleReports } from "./initial/ReportTargets/ConsoleReport";
+import { WinsAnalysis } from "./initial/Analysers/WinsAnalysis";
+import { Summary } from "./initial/Summary";
 // Create an Object that satifies the data reader interface
 const csvFileReader = new CsvFileReader("football.csv");
 // Creat an instance of Match Reader and pass in something satisfying the  'Datareader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
+
+const summary = new Summary(new WinsAnalysis('Tottenham'), new ConsoleReports());
+
+summary.buildAndPrintReport(matchReader.matches)
