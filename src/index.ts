e5785c1b45d313a8e37eede4d174809b30b394bd
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-03-17 01:18:20 +0100

    new index

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
-import { MatchReader } from "./initial/MatchReader";
-import { CsvFileReader } from "./initial/CsvFileReader";
-import { ConsoleReports } from "./initial/ReportTargets/ConsoleReport";
-import { WinsAnalysis } from "./initial/Analysers/WinsAnalysis";
-import { Summary } from "./initial/Summary";
-import { HTMLReport } from "./initial/ReportTargets/HTMLReport";
-// Create an Object that satifies the data reader interface
-const csvFileReader = new CsvFileReader("football.csv");
-// Creat an instance of Match Reader and pass in something satisfying the  'Datareader' interface
-const matchReader = new MatchReader(csvFileReader);
+import { MatchReader } from "./MatchReader";
+import { Summary } from "../initial/Summary";
+
+
+const matchReader = MatchReader.fromCSV('football.csv')
 matchReader.load();
 
 
-const summary = new Summary(new WinsAnalysis('Tottenham'), new HTMLReport());
+const summary = Summary.winAnylysisWithHtmlReport('Arsenal')
+// const summary = new Summary(new WinsAnalysis('Tottenham'), new HTMLReport());
 
 summary.buildAndPrintReport(matchReader.matches)
