12a98d1708aed327a2b72342e9b5333ad3d845e1
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-03-22 01:19:07 +0100

    static method on summary

--- src/initial/Summary.ts
+++ src/initial/Summary.ts
@@ -1,4 +1,6 @@
 import { MatchData } from "./MatchData";
+import { WinsAnalysis } from "./Analysers/WinsAnalysis";
+import { HTMLReport } from "./ReportTargets/HTMLReport";
 
 export interface Analyser {
     run (matches: MatchData[]):string;
@@ -9,6 +11,13 @@ export interface OutputTarget {
 }
 
 export class Summary {
+    static winAnylysisWithHtmlReport (team:string){
+        return new Summary(
+            new WinsAnalysis(team),
+            new HTMLReport(),
+        )
+    }
+    
  constructor(public analyser: Analyser, public outputTarget: OutputTarget){}
 
  buildAndPrintReport(matches: MatchData[]): void {
